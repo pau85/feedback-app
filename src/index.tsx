61acfbef72f833386b21ecbe38586966873e677b
Paulette Currington <paulette.currington@gmail.com>
2025-01-30 22:53:33 -0600

    index.html and tsx file adjustments

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container!);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const container = document.getElementById('root')
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />)
+}
