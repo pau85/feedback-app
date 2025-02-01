6f56b4191f1e367864539966f46d1aeb18556618
Paulette Currington <paulette.currington@gmail.com>
2025-02-01 15:51:08 -0600

    added another conditional for basename

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 const LazyFeedBack = React.lazy(() => import('./Components/Feedback/Feedback'));
 
-const Router: React.FC = () => (
-  <BrowserRouter basename="/">
+const Router: React.FC = () =>{
+  const basename = process.env.NODE_ENV === 'production' ? '/feedback-app' : '/';
+  return (
+  <BrowserRouter basename="basename">
     <Routes>
       <Route path="/" element={<LazyFeedBack />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </BrowserRouter>
 );
+}
 
 export default Router;
