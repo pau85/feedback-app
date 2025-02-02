8373cd2e17ad218d0b5e101faad0d5df1f762c4f
Paulette Currington <paulette.currington@gmail.com>
2025-02-02 15:31:22 -0600

    moved deploy to build directory

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: isProduction ? '/feedback-app/' : '/',
   },
